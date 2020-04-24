785c955630a83b4b28ab6ae7faa00b05a13d63ab
Jan <jan.ed.pachla@gmail.com>
2020-04-24 12:04:03 -0400

    Added controls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello Therea
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
       </div>
-    )
+    );
   }
 }
 
